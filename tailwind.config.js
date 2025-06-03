d38741a7b9786a1fcb8820570cefcaf6c88e4489
Adam S. <98590550+adamsochorec@users.noreply.github.com>
2025-06-03 13:30:20 +0200

    Flowbite integration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/views/**/*.html.slim",
+    "./app/assets/**/*.js",
+    "./app/assets/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
